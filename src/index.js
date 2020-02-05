3fe69fb183e1eafc8ee0d5de93cccca97412b862
Jordan Rocha <70rd23@gmail.com>
2020-02-05 10:27:15 -0300

    Add default style to app index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
-const App = () => <Routes />;
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    flex: 1,
+    backgroundColor: '#F5F6FC',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" backgroundColor="#F5F6FC" />
+    <Routes />
+  </View>
+);
 
 export default App;
