fa36e66e8b85beac626fe8709cc724fc046d77e6
Jordan Rocha <70rd23@gmail.com>
2020-02-10 12:40:19 -0300

    Added margin function

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -3,7 +3,7 @@ import { Container } from './styles';
 
 const Tile = props => {
   return (
-    <Container height={props.height} width={props.width}>
+    <Container mrg={props.margin} height={props.height} width={props.width}>
       {props.children}
     </Container>
   );
--- src/components/Tile/styles.js
+++ src/components/Tile/styles.js
@@ -1,9 +1,36 @@
 import styled from 'styled-components/native';
 
+const valueToPX = (value = 0) => {
+  return `${value}px`;
+};
+
+const setupMargin = (margin = '0') => {
+  margin = margin.split(',');
+  switch (margin.length) {
+    case 1:
+      return `margin: ${margin[0]}px;`;
+    case 2:
+      return `
+        margin-top: ${margin[0]}px;
+        margin-bottom: ${margin[0]}px;
+        margin-left: ${margin[1]}px;
+        margin-right: ${margin[1]}px;
+      `;
+    case 4:
+      return `
+        margin-top: ${margin[0]}px;
+        margin-bottom: ${margin[1]}px;
+        margin-left: ${margin[2]}px;
+        margin-right: ${margin[3]}px;
+      `;
+  }
+};
+
 export const Container = styled.View`
   background-color: ${props => props.theme.colors.white};
   border-radius: 16px;
   height: ${props => (props.height || 75) + 'px'};
   width: ${props => (props.width || 75) + 'px'};
   padding: 15px;
+  ${props => setupMargin(props.mrg)};
 `;
